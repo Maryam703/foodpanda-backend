805d32a1af17fb9536cfb006b3dd9f24577536df
maryam <maryam.rizwan10@icloud.com>
2024-12-25 18:15:44 +0500

    revert

--- api/index.js
+++ api/index.js
@@ -1,27 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import { dbConnect } from "./db/connect.js";
-// import appRoutes from "./app.js"
+import appRoutes from "./app.js"
 
 dotenv.config()
 
 const app = express({
-    path : "../.env"
+    path: "../.env"
 });
 
-// app.use("/", appRoutes)
+app.use("/", appRoutes);
 
-app.get('/',(req,resp)=>"<h1>I'm Running.</h1>")
 
 dbConnect()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`mongodb database working on port ${process.env.PORT}`)
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`mongodb database working on port ${process.env.PORT}`)
+        })
     })
-})
-.catch((error) => {
-    console.log(`mongodb connection error, ${error}`)
-})
+    .catch((error) => {
+        console.log(`mongodb connection error, ${error}`)
+    })
 
 
 export default app
